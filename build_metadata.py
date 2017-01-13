5dd5109ef346e00b8615b31dbc1ebf66e5de7d96
Chris Moore <chris.moore@makerbot.com>
2017-01-13 15:41:51 -0500

    Add build-root as a dependency
    We have to use a separate branch for the gfx drivers for now.

--- build_metadata.py
+++ build_metadata.py
@@ -11,4 +11,10 @@ else:
             version="4.*",
             debug=False,
             stable_required=True),
+        artifactory_utils.ArtifactSelector(
+            project="morepork-build-root",
+            revision="gfx_bringup",
+            version="*",
+            debug=False,
+            stable_required=True),
     ]
