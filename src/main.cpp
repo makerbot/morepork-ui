3ebe31e0ec364eb97101d7829f3308411e1b2c70
Chris Moore <chris.moore@makerbot.com>
2017-01-26 15:28:28 -0500

    Add .pro files for qt creator
    Now you can clone just this repo, open up the project file in qt
    creator and both develop and run the ui application there!  Just
    like you used to be able to with printerpanel (or so I hear tell).

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+// TODO: We should probably be able to set this up so that
+//       the qrc thing works for all builds...
+#ifdef MOREPORK_UI_QT_CREATOR_BUILD
+#define MOREPORK_UI_QML_MAIN QUrl("qrc:/main.qml")
+#else
+#define MOREPORK_UI_QML_MAIN "/usr/share/morepork_ui/main.qml"
+#endif
+
 int main(int argc, char ** argv)
 {
     QGuiApplication qapp(argc, argv);
 
-    // TODO: Find the qml file in a way that works for qt creator builds
-    QQmlApplicationEngine engine("/usr/share/morepork_ui/main.qml");
+    QQmlApplicationEngine engine(MOREPORK_UI_QML_MAIN);
 
     return qapp.exec();
 }
