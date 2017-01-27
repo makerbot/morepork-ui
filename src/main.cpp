39025cc66320a2d5c34b8f9a87a21fbc981d7151
Chris Moore <chris.moore@makerbot.com>
2017-01-27 16:59:47 -0500

    Fuck it, lets just display the actual IP address
    We'll get kaiten comms up soon enough, but why not display the actual
    IP address in the mean tie?

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 
+// TODO: Ditch this and use kaiten comms
+#include <QNetworkInterface>
+
 // TODO: We should probably be able to set this up so that
 //       the qrc thing works for all builds...
 #ifdef MOREPORK_UI_QT_CREATOR_BUILD
@@ -16,7 +19,13 @@ int main(int argc, char ** argv)
 
     QQmlApplicationEngine engine(MOREPORK_UI_QML_MAIN);
 
-    engine.rootContext()->setContextProperty("botIpAddr", "10.1.23.45");
+    for (auto addr : QNetworkInterface::allAddresses()) {
+        if (addr.protocol() == QAbstractSocket::IPv4Protocol &&
+                addr != QHostAddress::LocalHost) {
+            engine.rootContext()->setContextProperty("botIpAddr", addr.toString());
+            break;
+        }
+    }
 
     return qapp.exec();
 }
