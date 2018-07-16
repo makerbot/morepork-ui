63894157ece155a0dba6ca557a9c37d4155228e7
Shirley Du <shirley.du@makerbot.com>
2018-07-16 16:42:17 -0400

    Hide all boost logging stuff when MOREPORK_UI_QT_CREATOR_BUILD is defined

--- src/logging.h
+++ src/logging.h
@@ -1,10 +1,6 @@
 // Copyright MakerBot, Inc. 2017
 #ifndef SRC_LOGGING_HH_
 #define SRC_LOGGING_HH_
-#include <boost/log/sources/severity_channel_logger.hpp>
-#include <boost/log/trivial.hpp>
-
-#include <string>
 
 // A macro to use like BOOST_LOG_TRIVIAL that also does file and line info. In
 // conjunction
@@ -22,12 +18,17 @@
     qInfo() << "[" << __FILENAME__ << ":" << __LINE__ << ":" << __func__ << "] "\
             << "[TELEM] "
 #else
+
+#include <boost/log/sources/severity_channel_logger.hpp>
+#include <boost/log/trivial.hpp>
+
+#include <string>
+
 #define LOG(level) \
     BOOST_LOG_SEV(Logging::GeneralLog(), boost::log::trivial::level) << "["\
     << __FILENAME__ << ":" << __LINE__ << ":" << __func__ << "] "
 #define TELEM(level) \
     BOOST_LOG_SEV(Logging::TelemetryLog(), boost::log::trivial::level)
-#endif
 
 namespace Logging {
     typedef boost::log::sources
@@ -47,4 +48,5 @@ namespace Logging {
     telem_log& TelemetryLog();
     void Flush();
 };
+#endif
 #endif  // SRC_LOGGING_HH_
