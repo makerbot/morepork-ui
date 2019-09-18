b0b6893eb01946fc274d5440f23c1a73e35e438c
Praveen Rajendran <praveenr.2006@gmail.com>
2019-09-18 13:22:59 -0400

    Switch to simple file for storing DFS region
    BW-4902
    https://makerbot.atlassian.net/browse/BW-4902

--- src/dfs/dfs_settings.cpp
+++ src/dfs/dfs_settings.cpp
@@ -10,43 +10,28 @@
 DFSSettings::DFSSettings() :
     dfs_settings_path_(DFS_SETTINGS_PATH) {
     mkdir("/var/dfs", S_IFDIR);
-    std::ifstream dfs_str(dfs_settings_path_);
-    Json::Reader reader;
-    if (!dfs_str || !reader.parse(dfs_str, dfs_setting_)) {
-        dfs_setting_ = Json::Value();
-    }
 }
 
 void DFSSettings::loadDFSSetting() {
-    if (!dfs_setting_.isMember("region")) {
-        dfs_setting_["region"] = Json::Value();
+    std::ifstream dfs_str(dfs_settings_path_);
+    if(!dfs_str.is_open()) return;
+    std::string code;
+    std::getline(dfs_str, code);
+    dfs_str.close();
+    if (code == "00") {
         DFSRegionSet(DFS::Global);
-        updateDFSSetting(DFS::Global);
+    } else if (code == "KR") {
+        DFSRegionSet(DFS::Korea);
     } else {
-        Json::Value &region = dfs_setting_["region"];
-        if (region.isString()) {
-            const QString code = region.asString().c_str();
-            if (code == "00") {
-                DFSRegionSet(DFS::Global);
-            } else if (code == "KR") {
-                DFSRegionSet(DFS::Korea);
-            } else {
-                DFSRegionSet(DFS::Global);
-            }
-        }
+        DFSRegionSet(DFS::Global);
     }
 }
 
-
 void DFSSettings::updateDFSSetting(DFS region) {
-    Json::Value &dfs_region = dfs_setting_["region"];
-
-    dfs_region = Json::Value(dfs_region_str_[region]);
-
+    std::string code = dfs_region_str_[region];
     std::string tmp_path = dfs_settings_path_ + ".tmp";
-    std::ofstream tmp_str(tmp_path);
-    tmp_str << dfs_setting_.toStyledString();
-
+    std::ofstream tmp_str(tmp_path, std::ofstream::out);
+    tmp_str << code;
     // Hack to sync to disk since ofstream lacks flush() &
     // also there's no way to get the file descriptor from it.
     tmp_str.close();  // Force flush to filesystem buffer
