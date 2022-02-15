aed406abbc54ed5ab48068d20025f375c39e4e2b
Praveen Rajendran <praveenr.2006@gmail.com>
2022-02-15 19:12:47 +0530

    Update src/model/logger.cpp
    Co-authored-by: Chris Moore <chris.moore@makerbot.com>

--- src/model/logger.cpp
+++ src/model/logger.cpp
@@ -11,7 +11,7 @@ void Logger::info(QString msg) {
 
 QString Logger::getEnumName(QObject *object, QString name, int index) const {
     const QMetaObject* meta = object->metaObject();
-    int indexOfEnum = meta->indexOfEnumerator(&name.toStdString()[0]);
+    int indexOfEnum = meta->indexOfEnumerator(name.toStdString().c_str());
     if(indexOfEnum == -1) {
         qInfo() << "Enum not found; Check the itemWithEnum attribute for this swipeview";
         return QString(index);
