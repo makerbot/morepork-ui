5003f226cda147ea5e74902eb70dcd2b17a3ad21
Chris Moore <chris.moore@makerbot.com>
2023-06-02 03:13:58 -0400

    Update all of the ts files
    BW-5898
    http://ultimaker.atlassian.net/browse/BW-5898
    
    This removes obsolete translations from these files.  For the record the
    command I used to do this from a standard Toolchain-Release setup is:
    
    ../../Install/bin/lupdate -no-obsolete src/qml/*.qml -ts src/translations/*.ts

--- src/translations/translation_ar_EG.ts
+++ src/translations/translation_ar_EG.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:966c3178638c5ee5be3d6c06dbd491a89b3760cc99232c30839518612b46de53
-size 198930
+oid sha256:3bc18c0d51e40f0a8df1ea5cae9c1e7eb2fe2d102221e76e82610f13aa4ad30b
+size 236931
