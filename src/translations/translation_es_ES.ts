38e896fbae4851bf99ea1d6d76292dcbd7b97845
Shirley Du <shirley.du@makerbot.com>
2024-07-26 15:55:07 -0400

    update translations for print again

--- src/translations/translation_es_ES.ts
+++ src/translations/translation_es_ES.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90d23f2cb361e51f5322bbd2bca196a960fdb4996ded94c28b9a38c10d1b4172
-size 194951
+oid sha256:4b0a226840e07a1fca18f74ce0c44f8a3c9df0584381931c4a8cef3be5cb36ab
+size 171370
--- src/translations/translation_ko_KR.ts
+++ src/translations/translation_ko_KR.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b57ec9fe8f9ce701cacdb87e94b2813b3924f4336fefa937769318617cabd241
-size 193309
+oid sha256:1883d1e8e5dd9aee80dda7d261066e84084cb3b3bf718540205835dc7a6cff52
+size 169769
