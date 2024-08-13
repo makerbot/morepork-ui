13eefdec51cf66bfd37f4f15e52ce499719d7031
Shirley Du <shirley.du@makerbot.com>
2024-08-13 19:50:55 -0400

    filename for PrintIconForm got changed
    to PrintIcon, so update it in the .ts files so that the translations show up

--- src/translations/translation_fr_FR.ts
+++ src/translations/translation_fr_FR.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4dc7aabfdfc3782853591ea1605febdc4efe2da565c35bdb6d77fe3d2d7663f
-size 175556
+oid sha256:b34e5753fbbd01747e8de95f43eb1e8f32a02ebe8b3c3c14a3160dfb9e7e16d8
+size 175552
