2143132cbc0680bb22b6a282a6b8c0566105ddd2
Shirley Du <shirley.du@makerbot.com>
2024-09-16 14:01:16 -0400

    Accidentally exported from lokalize without comments last time (#765)
    Put the comments back and also fix wi-fi->Wi-Fi (the key in the qml has already
    been fixed)

--- src/translations/translation_fr_FR.ts
+++ src/translations/translation_fr_FR.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b34e5753fbbd01747e8de95f43eb1e8f32a02ebe8b3c3c14a3160dfb9e7e16d8
-size 175552
+oid sha256:b3da7dba67bb8e52a0e0a60f49972e71b1196e302e24d97b1f9fc74aeebb4200
+size 200936
